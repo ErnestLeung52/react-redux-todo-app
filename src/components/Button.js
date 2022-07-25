65b38566cfc8ab262180c1c1d69133cafebbaaf3
Ernest Leung <ernestleung52@gmail.com>
2022-07-25 13:50:09 -0700

    initial

---
+++ src/components/Button.js
@@ -0,0 +1,38 @@
+import React, { Children } from 'react';
+import styles from '../styles/modules/button.module.scss';
+import { getClasses } from '../utils/getClasses';
+
+const buttonTypes = {
+  primary: 'primary',
+  secondary: 'secondary',
+};
+
+function Button({ children, type, variant, ...rest }) {
+  return (
+    <button
+      // Conditionally render color based on variant passed in. need to invoke getClasses function to get the correct styling from scss
+      className={getClasses([
+        styles.button,
+        styles[`button--${buttonTypes[variant]}`],
+      ])}
+      type={type === 'submit' ? 'submit' : 'button'}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
+
+function SelectButton({ children, id, ...rest }) {
+  return (
+    <select
+      className={getClasses([styles.button, styles.button__select])}
+      {...rest}
+    >
+      {children}
+    </select>
+  );
+}
+
+export { SelectButton };
+export default Button;
